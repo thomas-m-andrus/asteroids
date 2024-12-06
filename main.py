4835a033e926f28635ec7b3a8c3e35072dbf699c
Thomas Andrus <thomasmandrus@gmail.com>
2024-12-05 20:25:00 -0600

    fixed

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    while(True): 
-        pygame.Surface.fill(screen,(0,0,0))
-        pygame.display.flip()
+
+    while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            dt = clock.tick(60)/1000
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
