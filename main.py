53329fd5d52a2ffd2631a5c729abb7c8a319de4a
Thomas Andrus <thomasmandrus@gmail.com>
2024-12-05 20:06:23 -0600

    created event loop

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True): 
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
