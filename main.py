3f8a181a00ae8f65a392a14ec9baa30955475b64
Thomas Andrus <thomasmandrus@gmail.com>
2024-12-17 15:36:01 -0600

    CH-3_1

--- main.py
+++ main.py
@@ -2,19 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
